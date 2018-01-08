bc9a329643d7fda6614710d14cd324b3325c4e15
zulnemekh <bekhbat_zulnemekh@unimedia.co.jp>
2018-01-08 11:59:09 +0800

    competition project is created by zulaa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Competition::Application.config.secret_key_base = '839a10fedc4ed008526c3b470ce1d2b5a65f53b66f34b0b05a830a623f05287cb21d39ce413aaeeaa71a71d80e741e9becb74e73f5a4dfb5f3a5f6c2d44f5ef1'
